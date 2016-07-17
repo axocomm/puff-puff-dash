70ad494bebb078e33baeac80e8da43678f981df6
axocomm <axocomm@gmail.com>
2016-07-16 20:34:43 -0400

    Add imgur embed and some image link rendering

--- src/cljs/puff_puff_dash/dashboards.cljs
+++ src/cljs/puff_puff_dash/dashboards.cljs
@@ -2,16 +2,38 @@
   (:require [reagent.core :as r]
             [reagent.session :as session]
             [puff-puff-dash.link-query :as lq]
-            [cognitect.transit :as t]))
+            [cognitect.transit :as t]
+            [clojure.string :as string]))
 
 (def dashboards
-  {:videos {:query {:where [{:cmp   :equals
-                             :field :properties.subreddit
-                             :value "videos"}]}}})
+  {:videos {:queries [{:where [{:cmp   :equals
+                                :field :properties.subreddit
+                                :value "videos"}]}
+                      {:where [{:cmp   :like
+                                :field :domain
+                                :value "youtube"}]}]}
+   :images {:queries [{:where [{:cmp   :equals
+                                :field :properties.subreddit
+                                :value "pics"}]}
+                      {:where [{:cmp   :like
+                                :field :domain
+                                :value "imgur"}]}]}})
 
 (defn query-for-dashboard [links type]
-  (when-let [query (:query (get dashboards type))]
-    (lq/apply-query {:clauses query} links)))
+  (when-let [queries (:queries (get dashboards type))]
+    (->> queries
+         (map #(lq/apply-query {:clauses %} links))
+         (apply concat)
+         set)))
+
+(defn image-link [{:keys [domain url] :as link}]
+  (if (= domain "i.imgur.com")
+    [:img {:src url}]
+    (let [imgur-id (last (string/split url #"/"))]
+      [:blockquote {:class "imgur-embed-pub"
+                    :lang  "en"
+                    :data  {:id      imgur-id
+                            :context false}}])))
 
 (defn videos-dashboard []
   [:div.container
@@ -24,5 +46,7 @@
   [:div.container
    (if-let [links (query-for-dashboard (session/get :links)
                                        :images)]
-     [:p (str "Got " (count links) " links")]
+     (for [link (take 10 (filter :url links))]
+       ^{:key (:id link)}
+       [image-link link])
      [:p "No links"])])
