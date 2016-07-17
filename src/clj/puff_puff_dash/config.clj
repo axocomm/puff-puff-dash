718b3752634ef96b540ed5d5c25b0cbd013fc7fb
axocomm <axocomm@gmail.com>
2016-07-17 12:18:55 -0400

    Add basic configuration JSON

--- src/clj/puff_puff_dash/config.clj
+++ src/clj/puff_puff_dash/config.clj
@@ -1,10 +1,18 @@
 (ns puff-puff-dash.config
   (:require [cprop.core :refer [load-config]]
             [cprop.source :as source]
-            [mount.core :refer [args defstate]]))
+            [mount.core :refer [args defstate]]
+            [clojure.data.json :as json]
+            [clojure.java.io :as io]))
+
+(defn from-json-config [filename]
+  (-> (io/resource filename)
+      slurp
+      (json/read-str :key-fn keyword)))
 
 (defstate env :start (load-config
                       :merge
                       [(args)
                        (source/from-system-props)
-                       (source/from-env)]))
+                       (source/from-env)
+                       (from-json-config "config.json")]))
